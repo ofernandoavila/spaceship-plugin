ba6cd2a69d231b16aec31c0391be5221e7307802
Fernando Avila <fernandoavilajunior@gmail.com>
2024-08-10 23:05:29 -0300

    versao instalada do admin e alteracao postbuild

--- admin-page/prepare_files.js
+++ admin-page/prepare_files.js
@@ -1,61 +1,57 @@
 // prepare_files.js
-const fs = require('fs');
-const path = require('path');
-const fastGlob = require('fast-glob');
+const fs = require("fs");
+const path = require("path");
+const fastGlob = require("fast-glob");
 
-const buildDir = path.resolve(__dirname, 'build/static');
-const destDir = path.resolve(__dirname, '../assets');
+const buildDir = path.resolve(__dirname, "build/static");
+const destDir = path.resolve(__dirname, "../assets");
 
 async function MoveJS() {
-  try {
-    const files = await fastGlob(`${buildDir}/js/main.*.js`);
+	try {
+		const files = await fs
+			.readdirSync(buildDir + "/js")
+			.filter((fn) => fn.startsWith("main") && fn.endsWith(".js"));
 
-    files.forEach(file => {
-      const fileNameWithHash = path.basename(file);
-      const newFileName = 'main.js';
-      const newFilePath = path.join(destDir + '/js', newFileName);
+		files.forEach(async (file) => {
+			if (!fs.existsSync(destDir + "/js")) {
+				fs.mkdirSync(destDir + "/js", { recursive: true });
+			}
 
-      if (!fs.existsSync(destDir + '/js')) {
-        fs.mkdirSync(destDir + '/js', { recursive: true });
-      }
+			fs.rename( buildDir + "/js/" + file, destDir + "/js/" + file, (err) => {
+				if (err) console.error("Erro ao mover arquivo:", err);
 
-      fs.rename(file, newFilePath, (err) => {
-        if (err) {
-          console.error('Erro ao mover e renomear o arquivo:', err);
-        } else {
-          console.log(`Arquivo ${fileNameWithHash} movido e renomeado para ${newFilePath}`);
-        }
-      });
-    });
-  } catch (err) {
-    console.error('Erro ao buscar arquivos:', err);
-  }
+				fs.rename(destDir + "/js/" + file, destDir + "/js/main.js", (err) => {
+					if (err) console.error("Erro ao renomear arquivo:", err);
+				});
+			});
+		});
+	} catch (err) {
+		console.error("Erro ao buscar arquivos:", err);
+	}
 }
 
 async function MoveCSS() {
-  try {
-    const files = await fastGlob(`${buildDir}/css/main.*.css`);
+	try {
+		const files = await fs
+			.readdirSync(buildDir + "/css")
+			.filter((fn) => fn.startsWith("main") && fn.endsWith(".css"));
 
-    files.forEach(file => {
-      const fileNameWithHash = path.basename(file);
-      const newFileName = 'main.css';
-      const newFilePath = path.join(destDir + '/css', newFileName);
+		files.forEach(async (file) => {
+			if (!fs.existsSync(destDir + "/css")) {
+				fs.mkdirSync(destDir + "/css", { recursive: true });
+			}
 
-      if (!fs.existsSync(destDir + '/css')) {
-        fs.mkdirSync(destDir + '/css', { recursive: true });
-      }
+			fs.rename( buildDir + "/css/" + file, destDir + "/css/" + file, (err) => {
+				if (err) console.error("Erro ao mover arquivo:", err);
 
-      fs.rename(file, newFilePath, (err) => {
-        if (err) {
-          console.error('Erro ao mover e renomear o arquivo:', err);
-        } else {
-          console.log(`Arquivo ${fileNameWithHash} movido e renomeado para ${newFilePath}`);
-        }
-      });
-    });
-  } catch (err) {
-    console.error('Erro ao buscar arquivos:', err);
-  }
+				fs.rename(destDir + "/css/" + file, destDir + "/css/main.css", (err) => {
+					if (err) console.error("Erro ao renomear arquivo:", err);
+				});
+			});
+		});
+	} catch (err) {
+		console.error("Erro ao buscar arquivos:", err);
+	}
 }
 
 MoveJS();
